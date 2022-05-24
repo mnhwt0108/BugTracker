eef79c4726ad6d92eae64f6c5e407347d1aeebf0
Nguyenle23___ <beenbin2308@gmail.com>
2022-05-24 20:18:51 +0700

    Update featured.jsx

--- src/components/featured/featured.jsx
+++ src/components/featured/featured.jsx
@@ -18,6 +18,8 @@ export default function Featured({ type, setGenre }) {
     }
   },[type]);
 
+  console.log(content);
+
   return (
     <div className="featured">
       {type && (
@@ -29,19 +31,19 @@ export default function Featured({ type, setGenre }) {
             onChange={(e) => setGenre(e.target.value)}
           >
             <option>Genre</option>
-            <option value="adventure">Adventure</option>
-            <option value="comedy">Comedy</option>
-            <option value="crime">Crime</option>
-            <option value="fantasy">Fantasy</option>
-            <option value="historical">Historical</option>
-            <option value="horror">Horror</option>
-            <option value="romance">Romance</option>
-            <option value="sci-fi">Sci-fi</option>
-            <option value="thriller">Thriller</option>
-            <option value="western">Western</option>
-            <option value="animation">Animation</option>
-            <option value="drama">Drama</option>
-            <option value="documentary">Documentary</option>
+            <option value="Adventure">Adventure</option>
+            <option value="Comedy">Comedy</option>
+            <option value="Crime">Crime</option>
+            <option value="Fantasy">Fantasy</option>
+            <option value="Historical">Historical</option>
+            <option value="Horror">Horror</option>
+            <option value="Romance">Romance</option>
+            <option value="Sci-fi">Sci-fi</option>
+            <option value="Thriller">Thriller</option>
+            <option value="Hornor">Hornor</option>
+            <option value="Action">Action</option>
+            <option value="Drama">Drama</option>
+            <option value="Documentary">Documentary</option>
           </select>
         </div>
       )}
